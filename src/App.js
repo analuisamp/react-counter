f3a3972f37cc6dcc90ff5c2db6f5cf2a0a22dbcd
ana luisa miranda <analuisamirandap@gmail.com>
2024-09-10 11:52:44 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+import React from 'react'
+
+export default function App() {
+  const [count, setCount ] = useState(0);
+
+  function handleAdd() {
+    setCount(count + 1)
+  }
+
+  function handleSub() {
+    setCount(count - 1)
+  }
+
+  function handleReset() {
+    setCount(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='box'>
+        <p>{count}</p>
+        <button onClick={handleAdd} className='add'>ADD</button>
+        <button onClick={handleSub} className='sub'>SUB</button>
+        <button onClick={handleReset} className='reset'>RESET</button>
+      </div>
     </div>
   );
 }
-
-export default App;
